cd71d1db8668152cc7c80a2d0d603a8fd1ed2ea9
Neva Wood <nevalu@gmail.com>
2016-10-26 15:33:00 -0700

    first commit

---
+++ ex2.py
@@ -0,0 +1,8 @@
+# a comment, so i can read my program later
+# anything after the # is ignored
+
+print ("i could have code like this") # and the comment after is ignored
+
+# you can also use comments to disable code
+#print ("this won't run")
+print ("this will run")
