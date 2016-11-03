a9c9f29174e23fadbfd946382248067d6e07a07c
Neva Wood <nevalu@gmail.com>
2016-11-03 16:17:45 -0700

    even shorter, 6 line ex17

--- ex17.py
+++ ex17.py
@@ -1,19 +1,11 @@
-from sys import argv
-from os.path import exists
-
-script, from_file, to_file, = argv
+from sys import argv; from os.path import exists; script, from_file, to_file, = argv
 
 print ("copying from %s to %s" % (from_file, to_file))
 
 indata = open(from_file).read()
 
-print ("the input file is %d bytes long" % len(indata), "\n", "does the output file exist? %r" % exists(to_file), "\n", "ready, hit RETURN to continue, CTRL-C to abort" )
-input()
+print ("the input file is %d bytes long" % len(indata), "\n", "does the output file exist? %r" % exists(to_file), "\n", "ready, hit RETURN to continue, CTRL-C to abort" ); input()
 
-out_file = open(to_file, 'w')
-out_file.write(indata)
+out_file = open(to_file, 'w').write(indata)
 
 print ("alright, all done")
-
-out_file.close()
-# in_file.close()
